e9de6b2e9dbd30a95aa89e506771c3462c3a2b67
nikolapivac <npivac00@fesb.hr>
2022-09-20 17:03:08 +0200

    Created input element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 import "./scss/App.scss";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="search_area">
+                <h1 className="search_area_title">Find your favorite Marvel characters!</h1>
+                <input type="text" placeholder="Search..."></input>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
